3d70b7e10682b007d40052ab894fae3b2d03502f
Jaime Garcia Garcia <jgarcia16432@gmail.com>
2020-03-05 22:11:35 -0800

    Add icons to the about page with styling

--- src/views/About.vue
+++ src/views/About.vue
@@ -10,22 +10,45 @@
       <h2 class="goals__heading">Our Goal</h2>
       <p class="goals__copy">Is to support artists, creators, and designers in their pursuit to create sustainable business practices, which will allow for personal and professional growth. Students will explore opportunities and learn how to solve problems by integrating design thinking, design sprints, and other creative processes. Students will design business strategies based on validated assumptions, using the feedback loop: design, measure, and learn.</p>
       <ul class="goals__list">
-        <li>Students will be encouraged to attend a series of lectures, workshops, and seminars to better familiarize themselves with business and business applications.</li>
-        <li>Students will tour and visit various businesses and will have an opportunity to learn and share their business models.</li>
-        <li>Students will be introduced to different levels of business development.</li>
+        <li class="goals__list-item">
+          Students will be encouraged to attend a series of lectures, workshops, and seminars to better familiarize themselves with business and business applications.
+          <img src="../assets/global/icon-check.svg" alt=""/>
+        </li>
+        <li class="goals__list-item">
+          Students will tour and visit various businesses and will have an opportunity to learn and share their business models.
+          <img src="../assets/global/icon-check.svg" alt=""/>
+        </li>
+        <li class="goals__list-item">
+          Students will be introduced to different levels of business development.
+          <img src="../assets/global/icon-check.svg" alt=""/>
+        </li>
       </ul>
       <img class="goals__image" src="../assets/global/air_rally_illustration_2.svg"/>
     </section>
     <section class="curricula">
       <h2 class="curricula__heading">Curricula</h2>
-      <p class="curricula__copy">The following is a short outline for new businesses. And there are three tiers of potential curriculums we can build. Through these hands-on curriculum options, students will start, refine, or expand their work for an entrepreneurial venture.</p>
+      <p class="curricula__copy-1">
+        The following is a short outline for new businesses. And there are three tiers of potential curriculums we can build.
+      </p>
+      <p class="curricula__copy-2">
+        Through these hands-on curriculum options, students will start, refine, or expand their work for an entrepreneurial venture.
+      </p>
       <dl class="curricula__def-list">
-        <dt>Idealist</dt>
-          <dd>First is the Idealist. These are individuals who may have a dream of opening up a  new business, but they really don’t have many resources or knowledge about how to start a new business.</dd>
-        <dt>Start-Ups</dt>
-          <dd>The second group is a pre-existing group of “Start-Ups.” They have some knowledge about business, but they need additional help.</dd>
-        <dt>Owners</dt>
-          <dd>The last group is comprised of existing business owners who simply want to expand or make their companies more sustainable.</dd>
+        <div class="curricula__list-wrapper">
+          <dt>Idealist</dt>
+            <dd>First is the Idealist. These are individuals who may have a dream of opening up a  new business, but they really don’t have many resources or knowledge about how to start a new business.</dd>
+            <img src="../assets/about/icon-cloud.svg"/>
+        </div>
+        <div class="curricula__list-wrapper">
+          <dt>Start-Ups</dt>
+            <dd>The second group is a pre-existing group of “Start-Ups.” They have some knowledge about business, but they need additional help.</dd>
+            <img src="../assets/about/icon-power.svg"/>
+        </div>
+        <div class="curricula__list-wrapper">
+          <dt>Owners</dt>
+            <dd>The last group is comprised of existing business owners who simply want to expand or make their companies more sustainable.</dd>
+            <img src="../assets/about/icon-suitcase.svg"/>
+        </div>
       </dl>
     </section>
     <section class="cooperation">
@@ -102,6 +125,19 @@ export default {
   &__list{
     grid-row: 4/5;
     padding-left: 5rem;
+    padding: 0;
+  }
+  &__list-item{
+    display: grid;
+    grid-template-columns: 8rem auto;
+    margin-bottom: 1rem;
+    img{
+      width: 3.5rem;
+      justify-self: start;
+      grid-column: 1/2;
+      grid-row: 1/2;
+      padding-top: .5rem;
+    }
   }
   &__image{
     grid-column: 2/3;
@@ -113,9 +149,9 @@ export default {
 }
 .curricula{
   display: grid;
-  grid-template-rows: 5rem auto auto 5rem;
+  grid-template-rows: 5rem auto auto auto 5rem;
   grid-template-columns: repeat(2, 1fr);
-  &__heading, &__copy{
+  &__heading, &__copy-1, &__copy-2{
     grid-column: 1/2;
     padding: 0 5rem;
   }
@@ -123,20 +159,48 @@ export default {
     grid-row: 2/3;
   }
   &__copy{
-    grid-row: 3/4;
+    &-1{
+      grid-row: 3/4;
+    }
+    &-2{
+      grid-row: 4/5;
+    }
   }
   &__def-list{
     grid-column: 2/3;
-    grid-row: 2/4;
+    grid-row: 2/5;
     padding: 0 5rem;
   }
-
   &::before{
     content: "";
     background: $yellow-hue-1;
     grid-column: 1/2;
-    grid-row: 1/5;
+    grid-row: 1/6;
     border-radius: 0 10px 10px 0;
+  }
+  &__list-wrapper{
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    grid-template-rows: repeat(2, auto);
+    margin-bottom: 1rem;
+    dt, dd{
+      grid-column: 2/7;
+    }
+    dt{
+      grid-row: 1/2;
+      font-weight: 700;
+    }
+    dd{
+      grid-row: 2/3;
+      margin: 0;
+    }
+    img{
+      grid-column: 1/2;
+      grid-row: 2/3;
+      width: 3.5rem;
+      justify-self: center;
+      padding-top: .5rem;
+    }
   }
 }
 .cooperation{
