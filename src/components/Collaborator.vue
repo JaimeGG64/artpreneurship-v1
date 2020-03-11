8c6fd71e004f39cf6c0a01735f5ae3aa1d8c0bf2
kylechan19 <51379831+kylechan19@users.noreply.github.com>
2020-03-11 12:35:29 -0700

    Create Collaborator.vue component I left in an example to see the usage of the component.

---
+++ src/components/Collaborator.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="collaborator">
+        <img id="pic" :src='filename' :alt='altTxt'>
+        <div id="name">{{ cName }}</div>
+        <div id="desc">{{ description }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        cName: String,
+        description: String,
+        filename: String,
+        altTxt: String,
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    @import "../scss/variables.scss";
+    @import "../scss/type.scss";
+
+    #name {
+        font-weight: 700;
+        font-size: 2rem;
+    }
+    #desc {
+        font-weight: 400;
+        font-size: 1rem;
+    }
+    
+</style>
--- src/views/Collaborators.vue
+++ src/views/Collaborators.vue
@@ -1,12 +1,42 @@
 <template>
     <main class="Collaborators">
         <h1>Collaborators Page</h1>
+        <div id="collaborators-people">
+            <ul v-for="(collaborator, index) in collaborators" :key=index>
+                <Collaborator 
+                    :cName="collaborator.name"
+                    :description="collaborator.description"
+                    :filename="collaborator.filename"
+                    :altTxt="collaborator.altTxt"
+                ></Collaborator>
+            </ul>
+        </div>
     </main>
 </template>
 
 <script>
+import Collaborator from '../components/Collaborator.vue'
+import DaveImg from '../assets/collaborators/dave-photo.jpg'
+
 export default {
     name: 'collaborators',
     title: 'Collaborators',
+
+    components: {
+        Collaborator,
+    },
+
+    data: function() {
+        return {
+            collaborators: [
+                {
+                    name: 'Dave Moon',
+                    description: 'Hello World!',
+                    filename: DaveImg,
+                    altTxt: 'iss dave',
+                },
+            ],
+        }
+    },
 }
 </script>
