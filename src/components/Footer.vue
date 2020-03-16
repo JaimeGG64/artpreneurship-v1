0daeea411ff67807766584faa49bf116bed39fde
kylechan19 <51379831+kylechan19@users.noreply.github.com>
2020-03-16 16:35:56 -0700

    Begin creating footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="footer-wrapper">
+        <div class="footer-wrapper__info">
+            <img alt="Artpreneurship logo" src="../assets/logo.png" class="footer-wrapper__logo">
+            <p>
+                18111 Nordhoff St,<br>
+                Northridge, CA, 91330
+            </p> 
+            <p>
+                +1 (818) 123-4567<br>
+                info@artpreneurship.com
+            </p>
+        </div>
+        <p class="footer-wrapper__copyright">© Copyright 2019. Artpreneurship All Right Reserved</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+@import "../scss/variables.scss";
+
+.footer-wrapper {
+    width: 100%;
+    &__logo {
+        width: 4.75rem;
+    }
+    &__info {
+        display: flex;
+        justify-content: space-evenly;
+        background: white;
+        color: $black-hue-1;
+        font-size: 1rem;
+        font-weight: 700;
+        font-family: $default-font;
+    }
+    &__copyright {
+        background: $black-hue-1;
+        background-size: 6.25rem;
+        color: white;
+        font-size: 1rem;
+        font-weight: 400;
+        font-family: $default-font;
+    }
+}
+</style>
