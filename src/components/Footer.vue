19802d8ac546990438401a3363c22ce2381ec32e
kylechan19 <51379831+kylechan19@users.noreply.github.com>
2020-03-18 09:16:43 -0700

    Display current year & link phone number + email address

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,22 +2,29 @@
     <div class="footer-wrapper">
         <section class="footer-wrapper__info">
             <img alt="Artpreneurship logo" src="../assets/logo.svg" class="footer-wrapper__logo">
-            <p class="footer-wrapper__info-address">
+            <p class="footer-wrapper__info--address">
                 18111 Nordhoff St,<br>
                 Northridge, CA, 91330
             </p> 
-            <p class="footer-wrapper__info-contact">
-                +1 (818) 123-4567<br>
-                info@artpreneurship.com
-            </p>
+            <section class="footer-wrapper__info--contact">
+                <a class="footer-wrapper__info--contact-phone" href="tel:8181234567">+1 (818) 123-4567</a><br>
+                <a class="footer-wrapper__info--contact-email" href="mailto:info@artpreneurship.com">info@artpreneurship.com</a>
+            </section>
         </section>
-        <p class="footer-wrapper__copyright">© Copyright 2019. Artpreneurship All Right Reserved</p>
+        <p class="footer-wrapper__copyright">© Copyright {{ year }}. Artpreneurship All Right Reserved</p>
     </div>
 </template>
 
 <script>
 export default {
-
+    data: function(){
+        return {
+            year: '',
+        }
+    },
+    created: function() {
+        this.year = new Date().getFullYear();
+    }
 }
 </script>
 
@@ -37,7 +44,7 @@ export default {
         padding: 5rem;
         color: $black-hue-1;
 
-        &-address, &-contact {
+        &--address, &--contact {
             @extend .copy;
         }
     }
