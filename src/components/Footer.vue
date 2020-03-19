dbf99337927768ff700764c7474e525d49e673c2
kylechan19 <51379831+kylechan19@users.noreply.github.com>
2020-03-19 10:14:46 -0700

    Finish mobile footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="footer-wrapper">
+    <footer class="footer-wrapper">
         <section class="footer-wrapper__info">
             <img alt="Artpreneurship logo" src="../assets/global/logo.svg" class="footer-wrapper__logo">
             <p class="footer-wrapper__info--address">18111 Nordhoff St,<br>Northridge, CA, 91330</p> 
@@ -9,12 +9,14 @@
             </section>
         </section>
         <section class="footer-wrapper__copyright">
-            <section class="footer-wrapper__copyright--social-media">
-                
+            <section class="footer-wrapper__copyright--logos">
+                <img class="footer-wrapper__copyright--logos-facebook" href="#" alt="Link to Facebook." src="../assets/global/facebook.svg"></img>
+                <img class="footer-wrapper__copyright--logos-twitter" href="#" alt="Link to Twitter." src="../assets/global/twitter.svg"></img>
+                <img class="footer-wrapper__copyright--logos-instagram" href="#" alt="Link to Instagram." src="../assets/global/instagram.svg"></img>
             </section>
             <p class="footer-wrapper__copyright--copy">© Copyright {{ year }}. Artpreneurship All Right Reserved</p>
         </section>
-    </section>
+    </footer>
 </template>
 
 <script>
@@ -37,7 +39,7 @@ export default {
 .footer-wrapper {
     display: grid;
     grid-template-columns: 100%;
-    grid-template-rows: 2;
+    grid-template-rows: repeat(2f, auto);
     box-shadow: 0 -.5rem 1.5rem rgba(0, 0, 0, 0.09);
     &__logo {
         width: 4.75rem;
@@ -45,7 +47,7 @@ export default {
     &__info {
         display: grid;
         grid-template-columns: 30% 70%;
-        grid-template-rows: 2;
+        grid-template-rows: repeat(2f, auto);
         background: white;
         color: $black-hue-1;
         padding: 3.5rem $mobile-gutter;
@@ -61,8 +63,22 @@ export default {
     }
     &__copyright {
         background: $black-hue-1;
+        display: grid;
+        grid-template-columns: 15% 70% 15%;
+        grid-template-rows: repeat(2f, auto);
         padding: 2rem $mobile-gutter;
-        &-copy{
+        padding-bottom: 1rem;
+        &--logos {
+            grid-row: 1/2;
+            grid-column: 2/3;
+            justify-self: center;
+            &-facebook, &-twitter, &-instagram {
+                padding: 0rem .3rem .9rem .3rem;
+            }
+        }
+        &--copy{
+            grid-column: 1/4;
+            grid-row: 2/2;
             width: 70%;
             font-size: .95rem;
             text-align: left;
