67dc7ef59d0ef92a4fe770540f5668754bf0b1b8
kylechan19 <51379831+kylechan19@users.noreply.github.com>
2020-03-28 17:47:24 -0700

    Make leeway on mobile collaborator component styling

--- src/components/Collaborator.vue
+++ src/components/Collaborator.vue
@@ -1,11 +1,16 @@
 <template>
-    <dl class=“curricula__def-list”>
-        <div class=“bio-wrapper”>
-            <img class=“bio-wrapper__img” :src='collabInfo.filename' :alt='collabInfo.altTxt'/>
-            <dt class=“bio-wrapper__name”>{{collabInfo.Name}}</dt>
-                <dd class=“bio-wrapper__desc”>{{collabInfo.description}}</dd>
-        </div>
-    </dl>
+    <section class="collab-wrapper__list__bio-wrapper">
+        <picture class="collab-wrapper__list__bio-wrapper__image">
+            <source media="(min-width: 1920px)" :srcset="collabInfo.lgFilename">
+            <source media="(min-width: 1024px)" :srcset="collabInfo.mdFilename">
+            <source media="(min-width: 640px)" :srcset="collabInfo.mobileFilename">
+                <img :src="collabInfo.mobileFilename" :alt="collabInfo.altTxt">
+        </picture>
+        <section class="collab-wrapper__list__bio-wrapper__copy">
+            <dt class=“collab-wrapper__list__bio-wrapper__copy__name”>{{collabInfo.Name}}</dt>
+            <dd class=“collab-wrapper__list__bio-wrapper__copy__desc”>{{collabInfo.description}}</dd>
+        </section>
+    </section>
 </template>
 
 <script>
@@ -17,26 +22,55 @@ export default {
 </script>
 
 <style lang="scss">
-    @import "../scss/variables.scss";
-    @import "../scss/type.scss";
+@import "../scss/variables.scss";
+@import "../scss/type.scss";
 
-    // .curricula {
-    //     &__def-list {
+.collab-wrapper {
+    &__list{
+        &__bio-wrapper {
+            box-shadow: 0 3px 20px rgba(0, 0, 0, .09);
+            width: 22.5rem;
+            height: 18.75rem;
+            display: grid;
+            grid-template-columns: 5% 90% 5%;
+            &__image {
+                grid-column: 1/4;
+            }
+            &__copy {
+                grid-column: 2/3;
+                // &__name {
 
-    //     }
-    // }
+                // }   
 
-    .bio-wrapper {
-        &__name {
-            font-weight: 700;
-            font-size: 2rem;
-        }
-
-        &__desc {
-            font-weight: 400;
-            font-size: 1.25rem;
+                &__desc {
+                    text-indent: 0;
+                }
+            }
         }
     }
-    
-    
+}
+
+@media screen and (min-width: map-get($break-point , "md")) {
+    .collab-wrapper {
+        &__list{
+            &__bio-wrapper {
+                box-shadow: 0 3px 20px rgba(0, 0, 0, .09);
+                width: 29rem;
+                height: 31.25rem;
+            }
+        }
+    }
+}
+
+@media screen and (min-width: map-get($break-point , "xl")) {
+    .collab-wrapper {
+        &__list{
+            &__bio-wrapper {
+                box-shadow: 0 3px 20px rgba(0, 0, 0, .09);
+                width: 29rem;
+                height: 31.25rem;
+            }
+        }
+    }
+}
 </style>
--- src/views/Collaborators.vue
+++ src/views/Collaborators.vue
@@ -1,19 +1,27 @@
 <template>
     <main class="Collaborators">
         <h1>Collaborators Page</h1>
-        <div class="collab-wrapper">
-            <ul class="collab-wrapper__list" v-for="(collaborator, index) in collaborators" :key=index>
-                <Collaborator class="collab-wrapper__collaborator"
-                    :collabInfo=collaborator
-                ></Collaborator>
-            </ul>
-        </div>
+        <section class="collab-wrapper">
+            <dl class="collab-wrapper__list" v-for="(collaborator, index) in collaborators" :key=index>
+                <Collaborator :collabInfo=collaborator></Collaborator>
+            </dl>
+        </section>
     </main>
 </template>
 
 <script>
 import Collaborator from '../components/Collaborator.vue'
-import DaveImg from '../assets/collaborators/dave-photo.jpg'
+
+import JaneMobile from '../assets/collaborators/jane-doe-mobile.png'
+import JohnMobile from '../assets/collaborators/john-doe-mobile.png'
+import JulieMobile from '../assets/collaborators/julie-doe-mobile.png'
+
+import JaneMd from '../assets/collaborators/jane-doe-md.png'
+import JohnMd from '../assets/collaborators/john-doe-md.png'
+
+import JaneDoe from '../assets/collaborators/jane-doe.png'
+import JohnDoe from '../assets/collaborators/john-doe.png'
+import JulieDoe from '../assets/collaborators/julie-doe.png'
 
 export default {
     name: 'collaborators',
@@ -29,18 +37,36 @@ export default {
         }
     },
     methods: {
-        pushCollaborator: function(title, desc, file, alt) {
+        pushCollaborator: function(title, desc, mobileFile, mdfile, file, alt) {
             var collab = {
                 Name: title,
                 description: desc,
-                filename: file,
+                mobileFilename: mobileFile,
+                mdFilename: mdfile,
+                lgFilename: file,
                 altTxt: alt,
             };
             this.collaborators.push(collab);
         },
     },
     beforeMount() {
-        this.pushCollaborator("Dave Moon", "Hello World!", DaveImg, "")
+        this.pushCollaborator("Jane Doe", "Hello World!", JaneMobile, JaneMd, JaneDoe, "")
+        this.pushCollaborator("John Doe", "Hello World!", JohnMobile, JohnMd, JohnDoe, "")
+        this.pushCollaborator("Julie Doe", "Hello World!", JulieMobile, JaneMd, JulieDoe, "")
     },
 }
 </script>
+
+<style lang="scss">
+@import "../scss/variables.scss";
+@import "../scss/type.scss";
+
+.collab-wrapper {
+    padding: 2rem $mobile-gutter;
+    &__list{
+        display: grid;
+        grid-template-columns: 100%;
+        padding: 1.25rem 0;
+    }
+}
+</style>
