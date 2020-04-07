41cc94511d56ab444c9f72f65a44fd4d8743c6f7
Jaime Garcia Garcia <jgarcia16432@gmail.com>
2020-04-07 00:52:27 -0700

    Style the global nav-bar for mobile and tablet view

--- src/components/GlobalNav.vue
+++ src/components/GlobalNav.vue
@@ -1,7 +1,13 @@
 <template>
     <nav class="global-nav">
-        <router-link to="/"><img alt="Artpreneurship logo" src="../assets/global/logo.svg" class="global-nav__logo"></router-link>
-        <ul class="global-nav__links">
+        <router-link to="/">
+            <img alt="Artpreneurship logo" src="../assets/global/logo.svg" class="global-nav__logo">
+        </router-link>
+        <button class="global-nav__btn" @click="toggleNavigation" :aria-expanded="isNavOpen ? 'true' : 'false'">
+            <img src="../assets/global/menu.svg" alt="Menu">
+        </button>
+        <ul class="global-nav__links" :class="isNavOpen ? 'global-nav__links--active' : ''">
+            <li class="global-nav__element"><router-link to="/">Home</router-link></li>
             <li class="global-nav__element"><router-link to="/about">About</router-link></li>
             <li class="global-nav__element"><router-link to="/collaborators">Collaborators</router-link></li>
             <li class="global-nav__element"><router-link to="/business">Business</router-link></li>
@@ -13,7 +19,27 @@
 
 <script>
 export default {
+    data () {
+        return {
+            isNavOpen: false,
+        }
+    },
 
+    methods: {
+        toggleNavigation() {
+            if (this.isNavOpen === false) {
+                this.isNavOpen = true
+            } else {
+                this.isNavOpen = false
+            }
+        }
+    },
+
+    watch: {
+        '$route' () {
+            this.isNavOpen = false
+        }
+    }
 }
 </script>
 
@@ -22,33 +48,88 @@ export default {
 
 .global-nav{
     width: 100%;
-    background: $black-hue-1;
+    background: $white;
     display: flex;
     justify-content: space-between;
-    overflow: hidden;
     padding: 1rem 1.75rem;
     box-sizing: border-box;
     &__logo{
         width: 4.75rem;
     }
+    &__btn{
+        border: none;
+        position: fixed;
+        top: 1rem;
+        right: 0;
+        padding: 1.75rem;
+        z-index: 100;
+        border-radius: 10px 0 0 10px;
+        background: $white;
+        cursor: pointer;
+    }
     &__links{
-        display:flex;
         list-style: none;
+        display:flex;
+        flex-direction: column;
         padding: 0;
         margin: 0;
+        position: fixed;
+        left: 0;
+        top: 0;
+        height: 0;
+        width: 0;
+        z-index: 50;
+        opacity: 0;
+        overflow: hidden;
+        transition: opacity .3s;
+        &--active{
+            background: rgba(255,255,255, .95);
+            height: 100vh;
+            width: 100vw;
+            justify-content: center;
+            box-sizing: border-box;
+            opacity: 1;
+        }
+    }
+    &__element {
+        height: 3rem;
+        width: 100vw;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        a{
+            color: $black-hue-3;
+            font-family: $default-font;
+            font-weight: $weight-regular;
+            text-transform: uppercase;
+            text-decoration: none;
+            text-align: center;
+            font-size: 1.8rem;
+            line-height: 0;
+        }
+    }
+        
+}
+
+.router-link-exact-active{
+    color: $black-hue-1 !important;
+    font-weight: $weight-bold !important;
+}
+
+@media screen and (min-width: map-get($break-point , "lg")) {
+.global-nav{
+    &__btn{
+        display: none;
+    }
+    &__links{
         width: 45rem;
         justify-content: space-evenly;
         align-items: center;
+        position: relative;
+        height: unset;
+        width: unset;
+        flex-direction: row;
     }
-    &__element a{
-        color: white;
-        font-family: $default-font;
-        font-weight: 700;
-        text-decoration: none;
-        font-size: 1.25rem;
-    }
-    a{
-        line-height: 0;
-    }
+}
 }
 </style>
