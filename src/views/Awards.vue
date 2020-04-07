a751a86a047edcbf9e07cb0b6dc6e5f9aac68bf2
Jaime Garcia Garcia <jgarcia16432@gmail.com>
2020-04-06 18:14:36 -0700

    Add content for the awards page

--- src/views/Awards.vue
+++ src/views/Awards.vue
@@ -1,6 +1,38 @@
 <template>
     <main class="Awards">
-        <h1>Awards Page</h1>
+        <section>
+            <h1>The Artpreneur Award</h1>
+            <p>The Artpreneur award is given, on an annual basis, to an Art and Design student who, in pursuing the study of art and design, has demonstrated the development of sustainable business practices. The Artpreneur Award is intended to encourage artists, designers, and creators to follow their passion and desire to create art…and to learn that the process of art-making has the power to create a sustainable business. Through art, creators and designers have an important role in society to make meaningful, positive contributions. It is in this spirit that the Artpreneur Award is given to a student who embodies an entrepreneur mindset.</p>
+            <p>The annual award is open to all art and design majors who are currently enrolled at CSUN. The following criteria will be utilized to decide on the student recipient. While studying art and design at CSUN, a student must have started an art-related business or have continued to develop a sustainable business, primarily utilizing their personal artwork, art-making, and other creative endeavors. The award is also meant for students to become motivated and encouraged to create a sustainable business with their art and art-making skills. The selected student will receive $2,000.00</p>
+            <a href="#">Apply Now</a>
+            <picture>
+                <source media="(min-width: 1280px)" srcset="../assets/awards/awards-working-xl.jpg">
+                <source media="(min-width: 1024px)" srcset="../assets/awards/awards-working-lg.jpg">
+                <source media="(min-width: 768px)" srcset="../assets/awards/awards-working-md.jpg">
+                <img src="../assets/awards/awards-working.jpg" alt="students working on the computer" style="width: 100%">
+            </picture>
+        </section>
+        <section>
+            <h2>The Following Items Should Be Included</h2>
+            <dl>
+                <div>
+                    <dt>Application Form & Faculty Letter</dt>
+                    <dd>Students should complete the application form and obtain one faculty letter of support.</dd>
+                    <img src="../assets/global/icon-check.svg" alt=""/>
+                </div>
+                <div>
+                    <dt>Presentation</dt>
+                    <dd>Submit a 3 to 5-page slide presentation providing information about their art business with visuals.</dd>
+                    <img src="../assets/global/icon-check.svg" alt=""/>
+                </div>
+                <div>
+                    <dt>Final Process</dt>
+                    <dd>The Scholarship Committee will identify the top three candidates and forward their files to the benefactor for the final decision.</dd>
+                    <img src="../assets/global/icon-check.svg" alt=""/>
+                </div>
+            </dl>
+            <img src="../assets/global/air_rally_illustration_5.svg" alt=""/>
+        </section>
     </main>
 </template>
 
