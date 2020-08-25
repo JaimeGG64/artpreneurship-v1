f41f9a75c3f8a73f5c10f322808e534b849dcaa8
Jaime Garcia Garcia <jgarcia16432@gmail.com>
2020-08-25 10:18:26 -0700

    Add more verbose variable names to collaborators.php

--- modules/collaborators.php
+++ modules/collaborators.php
@@ -4,23 +4,23 @@ $style = "";
 $classes = "";
 
 $repeater = $p['collaborator_container'];
-$order = array();
+$sorted = array();
 
-foreach ($repeater as $i => $row) {
-    $order[$i] = $row['name'];
+foreach ($repeater as $i => $sortBy) {
+    $sorted[$i] = $sortBy['name'];
 }
 
-array_multisort($order, SORT_ASC, $repeater);
+array_multisort($sorted, SORT_ASC, $repeater);
 
 ?>
 <section class="collab-list-section">
     <?php if ($repeater) : ?>
         <dl class="collab-list-section__list">
-            <?php foreach ($repeater as $i => $row) : ?>
+            <?php foreach ($repeater as $i => $display) : ?>
                 <div class="collab">
-                    <img class="collab__image" src="<?php echo $row['profile_picture']['url']; ?>" alt="<?php echo $row['profile_picture']['alt']; ?>">
-                    <dt class="collab__name"><?php echo $row['name']; ?></dt>
-                    <dd class="collab__desc"><?php echo $row['job_title']; ?></dd>
+                    <img class="collab__image" src="<?php echo $display['profile_picture']['url']; ?>" alt="<?php echo $display['profile_picture']['alt']; ?>">
+                    <dt class="collab__name"><?php echo $display['name']; ?></dt>
+                    <dd class="collab__desc"><?php echo $display['job_title']; ?></dd>
                 </div>
             <?php endforeach; ?>
         </dl>
