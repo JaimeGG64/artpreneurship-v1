5f8ad119bfd78f79713cbe3560641e48d3605761
Jaime Garcia Garcia <jgarcia16432@gmail.com>
2020-10-23 13:10:38 -0700

    Add color buttons to text-section

--- modules/text-section.php
+++ modules/text-section.php
@@ -9,6 +9,6 @@ $classes = "";
         echo $p['text_section_copy']
     ?>
     <?php if ($p['feature_button'] != 'none') : ?>
-        <a href="<?php echo $p['page_link']['url'] ?>" class="yellow-btn"><?php echo $p['link_text'] ?></a>
+        <a href="<?php echo $p['page_link']['url'] ?>" class="text-section__link text-section__link--<?php echo $p['feature_button_color'] ?>"><?php echo $p['link_text'] ?></a>
     <?php endif; ?>
 </section>
