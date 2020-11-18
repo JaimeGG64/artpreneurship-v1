e0fe0af14c5137a298744e71de7067f82a35ad8e
Jaime Garcia Garcia <jgarcia16432@gmail.com>
2020-11-17 22:58:27 -0800

    Add contact Information on the footer and add background images for the text-section module

--- footer.php
+++ footer.php
@@ -21,31 +21,31 @@
 	</div><!-- .site-info -->
 </footer><!-- #colophon -->
 
-<footer class="footer-wrapper">
-	<section class="footer-wrapper__info">
+<footer class="footer">
+	<div class="footer__info">
 		<?php the_custom_logo(); ?>
-		<p class="footer-wrapper__info--address">Northridge, CA, 91330</p>
-		<section class="footer-wrapper__info--contact"><a href="tel:8183995099" class="footer-wrapper__info--contact-phone">(818) 399-5099</a><br><a href="mailto:dave.moon@ixla.org?subject=Information About the Artpreneurship Program" class="footer-wrapper__info--contact-email">info@artpreneurship.com</a></section>
-	</section>
-	<section class="footer-wrapper__copyright">
-		<section class="footer-wrapper__copyright--logos"><a href="#"><img alt="Link to Facebook." src="img/facebook.svg" class="footer-wrapper__copyright--logos-facebook"></a><a href="#"><img alt="Link to Twitter." src="img/twitter.svg" class="footer-wrapper__copyright--logos-twitter"></a><a href="#"><img alt="Link to Instagram." src="img/instagram.svg" class="footer-wrapper__copyright--logos-instagram"></a>
+		<?php dynamic_sidebar('contact-sidebar') ?>
+	</div>
+	<section class="footer__copyright">
+		<section class="footer__copyright--logos">
+			<a href="#"><img alt="Link to Facebook." src="img/facebook.svg" class="footer__copyright--logos-facebook"></a><a href="#"><img alt="Link to Twitter." src="img/twitter.svg" class="footer__copyright--logos-twitter"></a><a href="#"><img alt="Link to Instagram." src="img/instagram.svg" class="footer__copyright--logos-instagram"></a>
 		</section>
-		<p class="footer-wrapper__copyright--copy">© Copyright <?php echo $year ?>. Artpreneurship All Rights Reserved</p>
+		<p class="footer__copyright--copy">© Copyright <?php echo $year ?>. Artpreneurship All Rights Reserved</p>
 		<?php
 		if (is_customize_preview()) :
 		?>
 			<p class="site-description"><?php echo $artpreneurship_v1_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
-?></p><a href="<?php echo esc_url(__('https://wordpress.org/', 'artpreneurship-v1')); ?>">
-<?php
-/* translators: %s: CMS name, i.e. WordPress. */
-printf(esc_html__('Proudly powered by %s', 'artpreneurship-v1'), 'WordPress');
-?>
-</a>
-<span class="sep"> | </span>
-<?php
-/* translators: 1: Theme name, 2: Theme author. */
-printf(esc_html__('Theme: %1$s by %2$s.', 'artpreneurship-v1'), 'artpreneurship-v1', '<a href="http://Kyle%20Chan">Jaime Garcia</a>');
-?>
+										?></p><a href="<?php echo esc_url(__('https://wordpress.org/', 'artpreneurship-v1')); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf(esc_html__('Proudly powered by %s', 'artpreneurship-v1'), 'WordPress');
+				?>
+			</a>
+			<span class="sep"> | </span>
+			<?php
+			/* translators: 1: Theme name, 2: Theme author. */
+			printf(esc_html__('Theme: %1$s by %2$s.', 'artpreneurship-v1'), 'artpreneurship-v1', '<a href="https://github.com/JaimeGG64">Jaime Garcia</a> and <a href="https://github.com/kylechan19">Kyle Chan</a>');
+			?>
 		<?php endif; ?>
 	</section>
 </footer>
--- modules/text-section.php
+++ modules/text-section.php
@@ -3,11 +3,42 @@ $style = "";
 $classes = "";
 ?>
 
-<section class="text-section">
+<style>
+    <?php if ($p['mobile_background']['url']) : ?>
+    .text-section-bg {
+        background-image: url("<?php echo $p['mobile_background']['url'] ?>");
+    }
+    <?php endif ?>
+    
+    <?php if ($p['tablet_background']['url']) : ?>
+    @media screen and (min-width: 768px) {
+        .text-section-bg {
+            background-image: url("<?php echo $p['tablet_background']['url'] ?>");
+        }
+    }
+    <?php endif ?>
+    
+    <?php if ($p['tablet_background']['url']) : ?>
+    @media screen and (min-width: 1024px) {
+        .text-section-bg {
+            background-image: url("<?php echo $p['large_background']['url'] ?>");
+        }
+    }
+    <?php endif ?>
+    <?php if ($p['tablet_background']['url']) : ?>
+    @media screen and (min-width: 1280px) {
+        .text-section-bg {
+            background-image: url("<?php echo $p['extra_large_background']['url'] ?>");
+        }
+    }
+    <?php endif ?>
+</style>
+
+<section class="text-section text-section-bg">
     <h2 class="text-section__heading">
         <?php echo $p['text_section_headline'] ?></h2>
-    <?php 
-        echo $p['text_section_copy']
+    <?php
+    echo $p['text_section_copy']
     ?>
     <?php if ($p['feature_button'] != 'none') : ?>
         <a href="<?php echo $p['page_link']['url'] ?>" class="text-section__link text-section__link--<?php echo $p['feature_button_color'] ?>"><?php echo $p['link_text'] ?></a>
